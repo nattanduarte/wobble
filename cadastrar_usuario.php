0a3da10fec0be50900b51b14793ec009d0e5f1dd
Nattan Duarte <nattanfd@gmail.com>
2017-09-26 15:09:05 -0300

    Apenas html

---
+++ cadastrar_usuario.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wobble</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="#">
+	<script src="./js/jquery.min.js"></script>
+</head>
+<body>
+	<header class="header">
+		<img src="#" alt="logo">
+		<ul>
+			<li><a href="#">Política de Privacidade</a></li>
+			<li><a href="#">Faça login</a></li>
+			<li><a href="#">Vamos rir um pouco?</a></li>
+		</ul>
+	</header>
+	<container class="content">
+		<form action="#" method="POST" class="formulario">
+			<input type="text" name="nome" placeholder="Nome">
+			<input type="text" name="snome" placeholder="Sobrenome">
+			<select name="sexo">
+				<option>Masculino</option>
+				<option>Feminino</option>
+			</select>
+			<input type="email" name="email" placeholder="E-mail">
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="senha" placeholder="Senha">
+			<input type="password" name="confsenha" placeholder="Confirmação da Senha">
+			<input type="submit">
+		</form>	
+	</container>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,31 @@
+<?php
+	if (isset($_SESSION['sessao'])) {
+?><header class="header">
+		<img src="#" alt="logo">
+		<ul>
+			<li><a href="#">Política de Privacidade</a></li>
+			<li><a href="#">Faça login</a></li>
+			<li><a href="#">Vamos rir um pouco?</a></li>
+		</ul>
+	</header>
+	<container class="content">
+		<img src="#" alt="Foto de perfil" class="perfil">
+		<img src="#" alt="foto de capa" class="capa">
+		<ul class="amigos"></ul>
+	</container>
+<?php  
+	}
+	else{
+		
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wobble</title>
+</head>
+<body>
+
+</body>
+</html>
