b47276759fe78343ee1716c14cce6d109b6d7ad1
José Vítor Heringer <josevitorheringer@gmail.com>
2017-10-05 18:43:47 -0300

    mostrando imagem do perfil e capa na página home

--- home.php
+++ home.php
@@ -2,6 +2,10 @@
 session_start();
 if (isset($_SESSION['usuario'])) {
 
+	$user = $_SESSION['usuario'];
+	$background = "./usuarios/" . $user . "/background.jpg";
+	$imagem = "./usuarios/" . $user . "/imagem.jpg";
+
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		session_unset();
 		session_destroy();
@@ -24,6 +28,15 @@ if (isset($_SESSION['usuario'])) {
 	<form action="#" method="post">
 		<input type="Submit" value="Encerrar Sessão">
 	</form>
-<?php } ?>
+<?php 
+	echo "<img id='capa' src='$background' alt='Cover'>";
+	echo "<img id='perfil' src='$imagem' alt='Cover'>";
+} 
+
+
+?>
+
+	
+
 </body>
 </html>
