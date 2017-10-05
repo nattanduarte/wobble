27d65db43ba51c069d6aa30f090290ba2652519c
Nattan Duarte <nattanfd@gmail.com>
2017-10-05 18:24:05 -0300

    estilos definidos, diretório criado e imagem salva

--- cadastrar_usuario.php
+++ cadastrar_usuario.php
@@ -26,11 +26,20 @@ else{
 		$password = hash('sha512', $password);
 		$passwordConfirm = hash('sha512', $passwordConfirm);
 
-		if ($password==$passwordConfirm) {
+		$foto=$_FILES['foto']['tmp_name'];
+		$background=$_FILES['background']['tmp_name'];
+	
 
+		
+
+		if ($password==$passwordConfirm) {
+			$caminho= mkdir("./usuarios/".$username."/",0777);
+			copy($foto,"./usuarios/".$username."/imagem.jpg");
+			copy($background,"./usuarios/".$username."/background.jpg");
 
 			$query="INSERT INTO cadastros VALUES ('$nome', '$sobrenome', '$sexo', '$email', '$username', '$password');";
 
+			
 			if (mysqli_query($conexao,$query)===TRUE){
 				header("Location: " . $_SERVER["HTTP_REFERER"] );		
 			} else {
@@ -49,37 +58,43 @@ else{
 <head>
 	<title>Wobble - Cadastre-se</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="#">
+	<link rel="stylesheet" type="text/css" href="./css/clear.css">
+	<link rel="stylesheet" type="text/css" href="./css/cadastrod.css">
+	<link rel="stylesheet" type="text/css" href="./css/estilo.css">
+	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
 	<script src="./js/jquery.min.js"></script>
 </head>
 <body>
 	<header class="header">
-		<img src="#" alt="logo">
-		<ul>
-			<li><a href="#">Política de Privacidade</a></li>
-			<li><a href="#">Faça login</a></li>
-			<li><a href="#">Vamos rir um pouco?</a></li>
-		</ul>
+		<h1 class="logo">Wobble</h1>
+		<nav class="menu">
+			<ul> 
+				<li><a href="#">Política de Privacidade</a></li>
+				<li><a href="index.php">Faça login</a></li>
+				<li><a href="#">Vamos rir um pouco?</a></li>
+			</ul>
+		</nav>	
 	</header>
 	<container class="content">
-		<form action="#" method="POST">
-			<input type="text" name="nome" placeholder="Nome"><br> 
-			<input type="text" name="sobrenome" placeholder="Sobrenome"><br>
-			Sexo
+		<form class="formu" action="#" method="POST"  enctype="multipart/form-data">
+			<label>Nome:</label><br/> <input type="text" name="nome" placeholder="Nome"> <br/>
+			<label>Sobrenome:</label><br/> <input type="text" name="sobrenome" placeholder="Sobrenome"><br/>
+			<label>Sexo: </label>
 			<select name="sexo">
 			  <option value="masculino">Masculino</option>
 			  <option value="feminino">Feminino</option>
-			</select><br>
-			<input type="email" name="email" placeholder="exemplo@mail.com"><br>
-			<input type="text" name="username" placeholder="Usuário"><br>
-			<input type="password" name="password" placeholder="Senha"><br>
-			<input type="password" name="passwordConfirm" placeholder="Confirmação de senha"><br>
-			<input type="file" name="fotoPerfil" ><br>
-			<input type="file" name="fotoBackground" ><br>
+			</select><br/>
+			<label>E-mail:</label><br/> <input type="email" name="email" placeholder="exemplo@mail.com"><br/>
+			<label>Username:</label><br/> <input type="text" name="username" placeholder="Usuário"><br/>
+			<label>Senha:</label><br/> <input type="password" name="password" placeholder="Senha"><br/>
+			<label>Confirme a senha:</label><br/> <input type="password" name="passwordConfirm" placeholder="Confirmação de senha"><br/>
+			<label>Foto de perfil:</label><br/> <input type="file" name="foto" ><br/>
+			<label>Foto de capa:</label><br/> <input type="file" name="background" ><br/>
 			<input type="submit" value="Cadastrar">
 		</form>
 		<br>
-		<button><a href="./index.php">Login</a></button>
+
 </container>
 </body>
 </html>
--- index.php
+++ index.php
@@ -56,32 +56,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
 	<title>Wobble - Login</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="#">
+	<link rel="stylesheet" type="text/css" href="./css/clear.css">
+	<link rel="stylesheet" type="text/css" href="./css/diagramacao.css">
+	<link rel="stylesheet" type="text/css" href="./css/estilo.css">
+	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
 	<script src="./js/jquery.min.js"></script>
 	
 </head>
 <body>
 	<header class="header">
-		<img src="#" alt="logo">
-		<ul>
-			<li><a href="#">Política de Privacidade</a></li>
-			<li><a href="cadastrar_usuario.php">Cadastre-se</a></li>
-			<li><a href="#">Vamos rir um pouco?</a></li>
-		</ul>
+		<h1 class="logo">Wobble</h1>
+		<nav class="menu">
+			<ul>
+				<li><a class="link" href="#">Política de Privacidade</a></li>
+				<li><a class="link" href="#">Vamos rir um pouco?</a></li>
+			</ul>
+		</nav>	
 	</header>
-	<section class="presentation">
-		<h1>Bem vindo ao Wobble</h1>
-		<p>The dog smells bad sniff other cat's butt and hang jaw half open thereafter for purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table. Ask to go outside and ask to come inside and ask to go outside and ask to come inside scratch the box chase laser cereal boxes make for five star accommodation meow for food, then when human fills food dish, take a few bites of food and continue meowing. Ask to go outside and ask to come inside and ask to go outside and ask to come inside sit in box so stare at ceiling light but kick up litter tuxedo cats always looking dapper meowing chowing and wowing. Hate dog spread kitty litter all over house, poop in litter box, scratch the walls chew on cable. Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff hide from vacuum cleaner and attack the dog then pretend like nothing happened climb a tree, wait for a fireman jump to fireman then scratch his face, and plan steps for world domination. Purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table stare at the wall, play with food and get confused by dust poop in litter box, scratch the walls but groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked pelt around the house and up and down stairs chasing phantoms yet kitty loves pigs for stares at human while pushing stuff off a table.</p>
-	</section>
-	<container class="content">
-		<form action="#" method="post">
-			<h2>Faça login</h2>
-			<input type="text" name="username" placeholder="Usuário" /><br>
-			<input type="password" name="password" placeholder="Senha" /><br>
-			<input type="submit" value="Entrar" />
-		</form>
-	<br>
-		<p>Não é cadastrado? <a href="cadastrar_usuario.php">Cadastre-se!</a></p>
+	<container class="principal">
+		<section class="presentation">
+			<h1>Bem vindo ao Wobble</h1>
+			<p>Essa rede social foi desenvolvida sob encomenda para você, usuário, que quer ter uma foto de perfil e uma de capa, adicionar uns amigos e tal!!</p>
+		</section>
+		<container class="content">
+			<form class="form" action="#" method="post">
+				<h2>Faça login</h2> 
+				<input type="text" name="username" placeholder="Usuário" /><br>
+				<input type="password" name="password" placeholder="Senha" /><br>
+				<input type="submit" value="Entrar" />
+			</form>
+		<br>
+			<p>Não é cadastrado? <a href="cadastrar_usuario.php">Cadastre-se!</a></p>
+		</container>
 	</container>
 </body>
 </html>
